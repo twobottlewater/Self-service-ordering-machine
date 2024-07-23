cc7342a8e2513a8663408ec74278fe9f3fe4e93e
parkin985 <yutao644@outlook.com>
2024-07-23 11:04:43 +0800

    更新了时间组件

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,45 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDate>
+#include <QIcon>
+#include <QMessageBox>
+#include <QTime>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    mtime=new QTimer(this);
+      mtime->start(1000); // 定时器每秒触发一次
+     connect(mtime, &QTimer::timeout, this, &MainWindow::update_time); // 连接定时器的timeout信号到update_time槽
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_payBt_clicked()
+{
+
+}
+
+void MainWindow::on_serverBt_clicked()
+{
+
+}
+
+void MainWindow::on_orderBt_clicked()
+{
+
+}
+
+//更新时间函数
+void MainWindow::update_time()
+{
+    ui->time->setText(QDate::currentDate().toString("yyyy-MM-dd")+" "+QTime::currentTime().toString("hh:mm:ss"));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,15 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QTime>
+#include <QDate>
+#include <QVector>
+#include "menu.h"
+#include <QDebug>//
+#include <QMap>//
+#include <QMessageBox>
+
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +24,24 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_payBt_clicked();
+
+    void on_serverBt_clicked();
+
+    void on_orderBt_clicked();
+     void update_time();
+
 private:
     Ui::MainWindow *ui;
+    QTimer *mtime;
+    QVector<menu*> varr;
+    int menuprice;//菜单价格
+    int dine_number;//餐桌人数
+    int table_number;//订单号
+    bool confirm;
+    bool ispayment;
+
 };
 
 #endif // MAINWINDOW_H
